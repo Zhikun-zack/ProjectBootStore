cf97785235a577ec0889ec5cb3feda3df2b1b80b
Zack <hzkzqxx@163.com>
2021-03-18 01:40:57 -0700

    Create a index.html

---
+++ src/main/java/com/bookstore/BookStoreNewApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreNewApplication.class, args);
+	}
+
+}
