c93e3491d566ac72451e2559241d117a741775bf
Zack <hzkzqxx@163.com>
2021-07-14 12:19:42 -0700

    '26'

---
+++ adminPortal/src/main/java/com/adminportal/AdminPortalApplication.java
@@ -0,0 +1,13 @@
+package com.adminportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPortalApplication.class, args);
+	}
+
+}
