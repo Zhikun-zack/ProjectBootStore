d87b0912ec3ecdc000cafc50afd3333727197ea4
Zack <hzkzqxx@163.com>
2021-07-14 16:12:43 -0700

    '27'

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,10 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home () {
+		return "home";
+	}
+}
