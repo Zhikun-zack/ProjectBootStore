ca430cabc3d1c1ab74ed4a7f2d6e9fc21f78ce10
Zack <hzkzqxx@163.com>
2021-07-31 22:01:07 -0700

    33

---
+++ adminPortal/src/main/java/com/adminportal/service/BookService.java
@@ -0,0 +1,11 @@
+package com.adminportal.service;
+
+import java.util.List;
+
+import com.adminportal.domain.Book;
+
+public interface BookService {
+	Book save(Book book);
+	
+	List<Book> findAll();
+}
